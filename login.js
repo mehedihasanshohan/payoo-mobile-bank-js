c99a05aec6bb001ff3dccdaf6a57b841fc4d5c5d
mehedi hasan <mhshohan04@gmail.com>
2025-07-05 17:40:26 +0600

    fix

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ document.getElementById('button-login').addEventListener('click', function(event
 
   if(phnNumber ==='019' && password=== '1234'){
     console.log('logged in');
-  window.location.href = '/home.html'
+  // window.location.href = '/home.html'
   window.location.href = 'pages/home.html';
 
   // window.location.href = '/https://mehedihasanshohan.github.io/payoo-mobile-bank-js/home.html'
