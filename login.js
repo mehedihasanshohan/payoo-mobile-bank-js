03d488037add1ba1f06e0fcb8c85ca3484008899
mehedi hasan <mhshohan04@gmail.com>
2025-07-05 11:33:16 +0600

    added add money form

--- login.js
+++ login.js
@@ -1,8 +1,21 @@
 document.getElementById('button-login').addEventListener('click', function(event){
   event.preventDefault();
-  console.log('clicked');
 
   const phnNumber = document.getElementById('phn-number').value;
   const password = document.getElementById('password').value;
   console.log(phnNumber,  password);
+
+  if(phnNumber ==='019' && password=== '1234'){
+    console.log('logged in');
+  window.location.href = '/home.html'
+
+  }
+  else{
+    alert('something wrong here');
+  }
+
+
+
+
+
 })
