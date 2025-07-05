b976ec023b350d406af3aefddec4ba99bb37b549
mehedi hasan <mhshohan04@gmail.com>
2025-07-05 15:48:55 +0600

    add money button functinalitty complete

---
+++ home.js
@@ -0,0 +1,28 @@
+const initialBalanceElement = document.getElementById('initial-balance');
+const initialBalance = Number(initialBalanceElement.innerHTML);
+
+document.getElementById('add-money-btn').addEventListener('click', function () {
+  const addMoneyQuantityStringInput = document.getElementById('str-amount');
+  const addMoneyQuantityString = addMoneyQuantityStringInput.value;
+  const addMoneyQuantity = Number(addMoneyQuantityString);
+  const pinInput = document.getElementById('pin');
+  const pin = pinInput.value;
+  console.log(pin, addMoneyQuantity, typeof(addMoneyQuantity));
+
+  if(pin=== '1234' && addMoneyQuantity>0){
+    const currentBalance = initialBalance + addMoneyQuantity ;
+    initialBalanceElement.innerHTML = currentBalance;
+
+    //  clear inputs
+    pinInput.value='';
+    addMoneyQuantityStringInput.value='';
+  }
+  else{
+    alert('wrong pin or amount');
+    //  clear inputs
+    pinInput.value='';
+    addMoneyQuantityStringInput.value='';
+  }
+
+
+});
